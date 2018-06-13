436c52408f89ca8fc4862a85b1bbc30ee8bea79f
DinushaNaveen <dldndasanayaka@gmail.com>
2018-06-13 15:36:37 +0530

    update app.js and packeg.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const classRoutes = require('./api/routes/classes');
 const pastpapersRoutes = require('./api/routes/pastpapers');
+
+app.use(morgan('dev'));
 
 app.use('/classes', classRoutes);
 app.use('/pastpapers', pastpapersRoutes);
