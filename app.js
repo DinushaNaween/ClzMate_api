825485192727a5bc884a6029976219b262805092
DinushaNaveen <dldndasanayaka@gmail.com>
2018-06-13 14:24:54 +0530

    update pastpapers.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 const classRoutes = require('./api/routes/classes');
+const pastpapersRoutes = require('./api/routes/pastpapers');
 
 app.use('/classes', classRoutes);
+app.use('/pastpapers', pastpapersRoutes);
 
 module.exports = app;
