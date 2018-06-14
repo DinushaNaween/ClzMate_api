dedf2f3bcf886222449827552f60a72fcfd0863f
DinushaNaveen <dldndasanayaka@gmail.com>
2018-06-14 09:43:07 +0530

    modify classes.GET and add mongoose

--- api/routes/classes.js
+++ api/routes/classes.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-router.get('/', (req, res, next)=>{
-    res.status(200).json({
-        message: 'GET'
-    });
+const Class = require('../models/class');
+
+router.get('/', (req, res, next) => {
+    Class.find()
+        .select('name price _id')
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            const response = {
+                count: docs.length,
+                classes: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'get',
+                            url: 'http://localhost:3000/classes/' + doc._id
+                        }
+                    }
+                })
+            }
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next)=>{
