ced163f918e109bfd3d2ae3dc9bf6530fcc83552
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-09-05 03:12:35 -0700

    test edit 2 (space)

--- api/routes/clzes.js
+++ api/routes/clzes.js
@@ -23,7 +23,7 @@ router.get('/', (req, res, next) => {
                         }
                     } 
                 })
-            }
+            } 
             res.status(200).json(response);
         })
         .catch(err => {
