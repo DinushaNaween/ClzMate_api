64e313c51fdb5b8bf04bfc9d40f5281e4e095fc1
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-09-12 09:34:47 -0700

    Deploying API

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./app')
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-const server = 'https://arcane-tundra-62265.herokuapp.com/'
+const server = http.createServer(app);
 
-//server.listen();        
+server.listen(port); 
