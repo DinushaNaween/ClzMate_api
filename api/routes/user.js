a01c118b8ec48ca6d27f53c6128f70287d3f0bcf
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-09-24 17:33:54 +0530

    user get edit

--- api/routes/user.js
+++ api/routes/user.js
@@ -22,6 +22,7 @@ router.get('/', (req, res) =>{
                 res.status(200).json({
                 Users: docs.map(doc => {
                     return{
+                        UserId: doc._id,
                         First_Name: doc.firstName,
                         Last_Name: doc.lastName,
                         Email: doc.email,
@@ -31,8 +32,20 @@ router.get('/', (req, res) =>{
                         Full_Name: doc.fullName,
                         Subjects: doc.subjects,
                         School: doc.school,
+                        AddressId: doc.address._id,
+                        First_Line: doc.address.firstLine,
+                        Second_Line: doc.address.secondLine,
                         City: doc.address.city,
-                        ContactDetails: doc.contactDetails
+                        District: doc.address.district,
+                        ContactDetailsId: doc.contactDetails._id,
+                        Land_Number: doc.contactDetails.landNumber,
+                        Mobile_Number: doc.contactDetails.mobileNumber,
+                        Mother_Name: doc.contactDetails.motherName,
+                        Mother_Number: doc.contactDetails.momNumber,
+                        Father_Name: doc.contactDetails.fatherName,
+                        Father_Number: doc. contactDetails.dadNumber,
+                        Gardian_Name: doc.contactDetails.gardianName,
+                        Gardian_Number: doc.contactDetails.gardianNumber
                     }
                 })
             })
--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const clzRoutes = require('./api/routes/clz');
 const paperRoutes = require('./api/routes/paper');
 const userRoutes = require('./api/routes/user');
 
-mongoose.connect('mongodb://admin:admin123@ds028559.mlab.com:28559/clzmate');
-//mongoose.connect('mongodb://localhost:27017/ClzMate');
+//mongoose.connect('mongodb://admin:admin123@ds028559.mlab.com:28559/clzmate');
+mongoose.connect('mongodb://localhost:27017/ClzMate');
 
 mongoose.Promise = global.Promise;
  
