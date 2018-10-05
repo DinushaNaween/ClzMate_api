f5b48629ebc2bda0ef7fb551618314dc455db9dd
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-10-05 13:05:33 +0530

    add attendance post route

--- api/routes/attendance.js
+++ api/routes/attendance.js
@@ -7,8 +7,30 @@ const clzController = require('../controllers/clzController');
 
 const Attendance = require('../models/attendance');
 
-router.post('/markAttendance', userController.findStudentById, clzController.findClzById, (req, res, next) => {
-    console.log('controllers OK');
+router.post('/newMonthAttendance', userController.findStudentById, clzController.findClzById, (req, res, next) => {
+    const attendance = new Attendance({
+        _id: new mongoose.Types.ObjectId(),
+        attendance: req.body.attendance,
+        clz: req.body.clz,
+        student: req.body.student,
+        cardMarker: req.body.cardMarker
+    });
+    console.log(attendance)
+    attendance
+        .save()
+        .then(attendance => {
+            res.status(200).json({
+                state: true,
+                attendance
+            })
+        })
+        .catch(err => {
+            res.status(200).json({
+                state: false
+            })
+        });
 });
+
+router.post('/addAttendance', )
 
 module.exports = router;
