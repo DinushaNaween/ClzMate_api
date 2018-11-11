54ae4567ff75b0a465e530adcc73d04aefca4781
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-11-11 22:46:05 +0530

    edit code

--- api/routes/user.js
+++ api/routes/user.js
@@ -392,7 +392,7 @@ router.get('/getStudentByClz/:clzId', (req, res, next) => {
     const clzId = req.params.clzId;
     User
         .aggregate([
-            { $match: { clzes: clzId } }
+            { $elemMatch: { clzes: clzId } }
         ])
         .then(list => {
             console.log(list);
