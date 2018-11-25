405174f181429c4582170e1eef5bd61e2631a28d
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-11-25 23:50:24 +0530

    edit on get all subjects

--- api/routes/subject.js
+++ api/routes/subject.js
@@ -11,7 +11,7 @@ router.get('/', (req, res, next) => {
         .exec()
         .then(subjects => {
             res.status(200).json({
-                subjects: subjects
+                Subject: subjects
             });
         })
         .catch(err => {
