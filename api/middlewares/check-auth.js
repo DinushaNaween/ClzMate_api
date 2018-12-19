709b538b825dbfb2a2c7035fac7bf26f1bd644e4
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-12-19 15:30:28 +0530

    edit decode function

--- api/middlewares/check-auth.js
+++ api/middlewares/check-auth.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 
 function decode(req, res, next){
-    const token = req.headers.token;
+    const token = req.headers.authorization.split(" ")[1];
     const decodeJWT = jwt.verify(token, process.env.JWT_KEY);
     return decodeJWT;
 }
