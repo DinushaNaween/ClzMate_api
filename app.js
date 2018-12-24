01e63f76a429e7c84a0b1eaa64f53d912a607936
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-12-24 20:54:07 +0530

    database changed

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const markRoutes = require('./api/routes/mark');
 const attendanceRoutes = require('./api/routes/attendance');
 const subjectRoutes = require('./api/routes/subject');
 
-// mongoose.connect('mongodb://admin:admin123@ds028559.mlab.com:28559/clzmate');
-mongoose.connect('mongodb://localhost:27017/ClzMate');
+mongoose.connect('mongodb://admin:admin123@ds028559.mlab.com:28559/clzmate');
+// mongoose.connect('mongodb://localhost:27017/ClzMate');
 
 mongoose.Promise = global.Promise;
  
