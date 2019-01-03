45bbc1a1bcfa98d461976c655c04f8cbaedf7256
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-01-04 00:48:35 +0530

    add .env file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// const dotenv = require('dotenv');
-// const result = dotenv.config()
+const dotenv = require('dotenv');
+const result = dotenv.config()
  
-// if (result.error) {
-//   throw result.error
-// }
+if (result.error) {
+  throw result.error
+}
 
 const http = require('http'); 
 const app = require('./app')
