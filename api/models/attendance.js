c068a98a616ce580c75f5114da45811e111e8c1b
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-01-20 12:01:03 +0530

    edit on attendance model

--- api/models/attendance.js
+++ api/models/attendance.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const attendanceSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     date: Date,
-    day : { type : Date, default: Date.now },
+    year: { type: String },
+    month: { type: String },
     clz: { type: mongoose.Schema.Types.ObjectId, ref: 'Clz' },
     student: [{ 
         type: mongoose.Schema.Types.ObjectId, 
