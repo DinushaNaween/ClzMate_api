8d37cd1f86b8ebaa515c530e64f44e7ed463cbdb
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-01-21 23:49:28 +0530

    edit payment post route

--- api/routes/payment.js
+++ api/routes/payment.js
@@ -5,7 +5,12 @@ const mongoose = require('mongoose');
 const Payment = require('../models/payment');
 
 router.post('/pay', (req, res, next) => {
+    console.log("Hello");
     console.log(req.body);
+    res.status(200).json({
+        message: "payment post",
+        result: req.body
+    })
 })
 
 module.exports = router; 
