0494e02671ec05559814a4ca66901d02b4bffb98
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-01-21 23:47:40 +0530

    basic testing for payhere checkout

--- api/routes/payment.js
+++ api/routes/payment.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
+const Payment = require('../models/payment');
 
+router.post('/pay', (req, res, next) => {
+    console.log(req.body);
+})
 
 module.exports = router; 
