d69c5db42683fb443fbe9a585a03969d638e92e4
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-01-23 16:58:43 +0530

    testing custom population

--- api/routes/attendance.js
+++ api/routes/attendance.js
@@ -170,7 +170,7 @@ router.get('/attendanceForClzId/:year/:month/:clzId', clzController.findClzIfExi
         .find({
             $and: [ { clz: clzId }, { year: reqYear } ]
         })
-        .populate('clz cardMarker')
+        .populate('clz.clzNo cardMarker.fullName')
         .exec()
         .then(attendanceList => {
             console.log(attendanceList);
