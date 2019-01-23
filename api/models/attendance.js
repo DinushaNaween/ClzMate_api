a7713afefdaa350d3b04463f3ff294d18ca8293b
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-01-23 19:12:16 +0530

    change variable types

--- api/models/attendance.js
+++ api/models/attendance.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const attendanceSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     date: Date,
-    year: { type: String },
-    month: { type: String },
+    year: { type: Number },
+    month: { type: Number },
     clz: { type: mongoose.Schema.Types.ObjectId, ref: 'Clz' },
     student: [{ 
         type: mongoose.Schema.Types.ObjectId, 
