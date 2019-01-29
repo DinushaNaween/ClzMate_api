e2c0d2b06015e0e04c946e3062566bc521abd37b
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-01-30 04:33:29 +0530

    toISOString()

--- api/routes/attendance.js
+++ api/routes/attendance.js
@@ -150,11 +150,11 @@ router.get('/studentAttendance/:studentId', (req, res, next) => {
         .then(result => {
             const length = result.length;
             console.log(result.length);
-            const dateIs = result[0].date
+            const dateIs = result[0].date.toISOString()
             console.log(dateIs)
             const attendedDates = [{ date: Date, clz: mongoose.Schema.ObjectId}];
             for (i=0 ; i<length ; i++ ){
-                attendedDates[i] = result[i].date
+                attendedDates[i] = result[i].date.toISOString()
             }
             res.status(200).json({
                 // date: result[0].date
